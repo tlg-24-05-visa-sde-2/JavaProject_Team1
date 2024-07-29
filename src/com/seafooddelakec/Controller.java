427d20d0e4b58331a4acd5da1ac2b21887511c47
Cortney B <cortneyb@tlgcohort.com>
2024-07-29 18:36:04 -0400

    Created base packages and layout fleshing/classes

---
+++ src/com/seafooddelakec/Controller.java
@@ -0,0 +1,62 @@
+package com.seafooddelakec;
+
+class Controller {
+
+    /* intro()
+     * restaurant name displays,
+     * customer meets host who prompt user to input their name,
+     *
+     * table()
+     * Customer is seated and menu is read and displayed from a csv file
+     * customer meets server
+     *
+     *
+     * Menu items will include prices (potentially using if statements)
+     *
+     * user drink & combo selection prompt
+     * user enters drink & combo codes for each selection,
+     * Prompt1: DRINKS ex. [1]water [2]soda [3]tequila
+     * Prompt2: COMBOS [1]shrimp,corn,potato [2]..
+     *
+     *
+     * after first selection is made, customer has option to reorder another selection
+     * (repeat previous prompts)
+     *
+     * or end order selection (menu will leave screen)
+     * server sends order to the kitchen
+     *
+     * customer waits for food (i.e demo loading screen)
+     * customer eats food [ -- keep eating, press E] *** if we have time to implement
+     *
+     * server delivers customer their bill
+     * (behind the scenes): each menu item has prices, the bill is calculated by adding its total items
+     * user is shown total
+     * and then being prompted to tip after
+     * the tip amounts: NONE, OKAY, GREAT, EXCELLENT
+     * the new total is displayed to customer
+     *
+     * server responds based on the tip amount selected
+     *
+     * farewell()
+     *
+     */
+
+    // temporary
+    private void intro() {
+    };
+
+    private void table() {
+    };
+
+    private void reorder() {
+    };
+
+    private void bill() {
+    };
+
+    private void serverEmoji() {
+    };
+
+    private void farewell() {
+    };
+}
---
+++ src/com/seafooddelakec/Intro.java
@@ -0,0 +1,5 @@
+package com.seafooddelakec;
+
+class Intro {
+
+}
