d29c5625e6ef4acca09d6bc6d5762af8b9bc4a83
DeShon Dixon <deshondixon@gmail.com>
2024-08-05 08:35:07 -0500

    Removed build.cmd, build.sh, run.cmd, run.sh, seadfooddelakec-1.0.jar file

--- build.sh
+++
@@ -1,3 +0,0 @@
-javac -d classes -classpath "lib/*" src/com/seafooddelakec/client/*.java src/com/seafooddelakec/app/*.java src/com/seafooddelakec/menu/*.java src/com/seafooddelakec/*.java
-
-jar --create --file seafooddelakec-1.0.jar -C classes .
--- run.sh
+++
@@ -1,1 +0,0 @@
-java -classpath seafooddelakec-1.0.jar:"lib/*" com.seafooddelakec.client.Main
